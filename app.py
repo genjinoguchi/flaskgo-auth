b45b79a99eefc28507bf288e638f448fa4a37ac5
Genji Noguchi <genji.noguchi@gmail.com>
2014-10-30 12:20:58 -0400

    init

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-from Flask import flask, redirect, render_template
+from Flask import flask, redirect, render_template, request
+import pygoogle
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+
+
+
+if __name__ == "__main__":
+    app.debug=True
+    app.run()
+
+
