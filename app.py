b68a1726b18ea63f1a84984b54af4d5e42974de1
Genji Noguchi <genji.noguchi@gmail.com>
2014-11-01 21:38:01 -0400

    framework

--- app.py
+++ app.py
@@ -1,18 +1,31 @@
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, request, make_response
 import pymongo
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
 
-@app.route("/")
+@app.route("/") #Register page, login page, 
 def index():
     return render_template("index.html")
 
+@app.route("/new-user",methods=["GET","POST"]):
+def create():
+	if request.method == "GET":
+
+		resp = make_response(render_template("index.html")) #Send the "create new user" page
+
+	if request.method == "POST": #look for post requests of new users
+		username = request.form["username"]
+		password = request.form["password"]
+		school = request.form["school"]
+		grade = request.form["grade"]
+
+		#Store it in mongo
+
+		#Send the response page
 
 
 if __name__ == "__main__":
     app.debug=True
     app.run()
-
-
